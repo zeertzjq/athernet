c667cfa05a497f921f2cad62e6ff83227cce3686
zeertzjq <zeertzjq@outlook.com>
2022-11-21 17:48:59 +0800

    Shorten shell script

--- random.sh
+++ random.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in {1..10}; do tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' < /dev/urandom | head -c 20; echo; sleep 1; done
+for i in {1..10}; do tr -dc '!-~' < /dev/urandom | head -c 20; echo; sleep 1; done
