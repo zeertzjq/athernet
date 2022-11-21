b6ca14e6d4f5144c33ec570d7d68bd2413e78929
zeertzjq <zeertzjq@outlook.com>
2022-11-21 14:03:23 +0800

    Add raw_recv

---
+++ raw_recv.c
@@ -0,0 +1,62 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+
+int main(int argc, char **argv) {
+  if (argc <= 1) {
+    fprintf(stderr, "Missing argument\n");
+    return EXIT_FAILURE;
+  }
+
+  struct sockaddr_in bind_addr = {
+      .sin_family = AF_INET,
+      .sin_port = 0,
+  };
+  if (inet_pton(AF_INET, argv[1], &bind_addr.sin_addr) == 0) {
+    fprintf(stderr, "Invalid IP address: %s\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+
+  int socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+  if (socket_fd < 0) {
+    perror(NULL);
+    return EXIT_FAILURE;
+  }
+  if (bind(socket_fd, (struct sockaddr *)&bind_addr, sizeof(bind_addr)) < 0) {
+    perror(NULL);
+    return EXIT_FAILURE;
+  }
+
+  for (int i = 0; i < 10; i++) {
+    struct sockaddr_in src_addr;
+    socklen_t addrlen = sizeof(src_addr);
+    char raw_payload[sizeof(struct iphdr) + sizeof(struct udphdr) +
+                     sizeof(uint32_t)];
+    if (recvfrom(socket_fd, &raw_payload, sizeof(raw_payload), 0,
+                 (struct sockaddr *)&src_addr, &addrlen) < 0) {
+      perror(NULL);
+      continue;
+    }
+    char addr[INET_ADDRSTRLEN];
+    if (inet_ntop(AF_INET, &src_addr.sin_addr, addr, sizeof(addr)) == NULL) {
+      perror(NULL);
+      continue;
+    }
+    char *ip_payload = raw_payload + sizeof(struct iphdr);
+    struct udphdr *udp_hdr_p = (struct udphdr *)ip_payload;
+    char *udp_payload = ip_payload + sizeof(struct udphdr);
+    printf("Received IP: %s, Source Port: %hu, Dest Port: %hu, Payload: %u\n",
+           addr, ntohs(udp_hdr_p->source), ntohs(udp_hdr_p->dest),
+           ntohl(*(uint32_t *)udp_payload));
+  }
+
+  return EXIT_SUCCESS;
+}
