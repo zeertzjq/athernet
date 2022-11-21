0bbafe5ab53abf0b1607f86c296d8fa1af592a3c
zeertzjq <zeertzjq@outlook.com>
2022-11-21 13:25:46 +0800

    Separate UDP send and recv

---
+++ udp_send.c
@@ -0,0 +1,62 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <time.h>
+
+int main(int argc, char **argv) {
+  if (argc <= 1) {
+    fprintf(stderr, "Missing argument\n");
+    return EXIT_FAILURE;
+  }
+
+  char *port_str = strchr(argv[1], ':');
+  if (port_str == NULL) {
+    fprintf(stderr, "Missing port number\n");
+    return EXIT_FAILURE;
+  }
+  int port = atoi(port_str + 1);
+  if (port < 0 || port > UINT16_MAX) {
+    fprintf(stderr, "Invalid port number: %d\n", port);
+    return EXIT_FAILURE;
+  }
+
+  *port_str = '\0';
+  char *addr = argv[1];
+  struct sockaddr_in dest_addr = {
+      .sin_family = AF_INET,
+      .sin_port = htons(port),
+  };
+  if (inet_pton(AF_INET, addr, &dest_addr.sin_addr) == 0) {
+    fprintf(stderr, "Invalid IP address: %s\n", addr);
+    return EXIT_FAILURE;
+  }
+
+  int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (socket_fd < 0) {
+    perror(NULL);
+    return EXIT_FAILURE;
+  }
+
+  for (int i = 0; i < 10; i++) {
+    struct timespec ts = {
+        .tv_sec = 1,
+    };
+    nanosleep(&ts, NULL);
+    uint32_t payload_h = time(NULL);
+    uint32_t payload_n = htonl(payload_h);
+    if (sendto(socket_fd, &payload_n, sizeof(payload_n), 0,
+               (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0) {
+      perror(NULL);
+      continue;
+    }
+    printf("Sent Payload: %u\n", payload_h);
+  }
+
+  return EXIT_SUCCESS;
+}
